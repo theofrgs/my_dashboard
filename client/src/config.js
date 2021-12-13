7518ee53c3b2aab92b2d63401e16593bcb8bb2f8
theo.frgs <theo.fargeas@epitech.eu>
2021-12-13 19:07:53 +0100

    Update env environement for google api

--- client/src/config.js
+++ client/src/config.js
@@ -1,25 +1,12 @@
-import configJson from "./auth_config.json";
-
-const dotenv = require('dotenv');
+import dotenv from "dotenv";
 
 dotenv.config();
 const env = {
   WEATHER_API_KEY: process.env.REACT_APP_WEATHER_API_KEY,
   CURRENCY_API_KEY: process.env.REACT_APP_CURRENCY_API_KEY,
   STEAM_API_KEY: process.env.REACT_APP_STEAM_API_KEY,
+  GOOGLE_CLIENT_ID: process.env.REACT_APP_GOOGLE_CLIENT_ID,
+  GOOGLE_CLIENT_KEY: process.env.REACT_APP_GOOGLE_CLIENT_KEY,
 }
 
 export {env};
-
-export function getConfig() {
-  const audience =
-    configJson.audience && configJson.audience !== "YOUR_API_IDENTIFIER"
-      ? configJson.audience
-      : null;
-
-  return {
-    domain: configJson.domain,
-    clientId: configJson.clientId,
-    ...(audience ? { audience } : null),
-  };
-}
