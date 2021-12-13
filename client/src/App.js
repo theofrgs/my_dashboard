c1fb3af2eddcd9230e1e59e5690a677ce5a4f215
theo.frgs <theo.fargeas@epitech.eu>
2021-12-13 19:07:01 +0100

    Redirect home to login page

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,22 @@
 import './App.css';
 import LoginPage from './views/LoginPage/LoginPage'
 import DashboardPage from './views/DashboardPage/DashboardPage'
-import Home from './components/Home'
 import RegisterPage from './views/RegisterPage/RegisterPage'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 
-function App({user}) {
-  return (
-    <Router>
-      <div className="App">
-        <Routes>
-          <Route path='/' element={<Home user={user}/>} />
-          <Route path='/login' element={<LoginPage user={user}/>} />
-          <Route path='/register' element={<RegisterPage user={user}/>} />
-          <Route path='/dashboard' element={<DashboardPage user={user}/>} />
-        </Routes>
-      </div>
-    </Router>
-  );
+function App() {
+    return (
+        <Router>
+            <div className="App">
+                <Routes>
+                    <Route path='' element={<Navigate to ="/login" />} />
+                    <Route path='/login' element={<LoginPage />} />
+                    <Route path='/register' element={<RegisterPage />} />
+                    <Route path='/dashboard' element={<DashboardPage />} />
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -4,9 +4,8 @@ import { Link } from 'react-router-dom';
 function Nav() {
     return (
         <div className="App">
-            <Link to='/' className="nav-item nav-link active">Home</Link>
-            <Link to='/register' className="nav-item nav-link active">RegisterPage</Link>
             <Link to='/login' className="nav-item nav-link active">LoginPage</Link>
+            <Link to='/register' className="nav-item nav-link active">RegisterPage</Link>
         </div>
     );
 }
